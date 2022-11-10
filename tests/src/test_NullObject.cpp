2406c0786454af7e8a3660a2f4fd67a18187662e
Dominik Meier <dominik.meier@student.hpi.de>
2022-11-10 15:09:59 +0100

    lint

--- tests/src/test_NullObject.cpp
+++ tests/src/test_NullObject.cpp
@@ -1,5 +1,3 @@
 #include <gtest/gtest.h>
 
-TEST(NullObjectTest, AssertReturnsSpace) {
-  EXPECT_STREQ("hello", "hello");
-}
+TEST(NullObjectTest, AssertReturnsSpace) { EXPECT_STREQ("hello", "hello"); }
