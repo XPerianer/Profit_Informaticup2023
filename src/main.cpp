0821a4dcf7f77292a156e0c4d34424098c798d12
Richard Ebeling <He3lixxx@users.noreply.github.com>
2022-11-17 10:36:13 +0100

    Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 
 #include "parsing.hpp"
+#include "placeable.hpp"
+#include "two_dimensional_vector.hpp"
+#include "utils.hpp"
 
 int main() {
   parsing::Input input = parsing::parse(std::cin);
@@ -29,6 +32,9 @@ int main() {
 *       -> gegeben N deposits, Designe Pipelines und Factory, um die Produktion zu realisieren
 */
 
+
+#include <queue>
+
 namespace production_realization {
   // Idee: Immer erstmal nur eine Produktionspipeline (-> genau eine Fabrik) bauen
   // Mehrere Fabriken dienen nur der Latenzoptimierung
@@ -41,5 +47,42 @@ namespace production_realization {
 
   bool attempt_realize(const std::vector<Deposit>& deposits, Factory::TypeT factory_type) {
     return false;
+  }
+
+  enum FieldOccupancy {
+    EMPTY,
+    BLOCKED,
+    CONVEYOR_CROSSING,
+    INPUT,
+    OUTPUT,
+  };
+
+  using OccupancyMap = TwoDimensionalVector<FieldOccupancy>;
+
+  std::optional<std::vector<PlaceableObject>> connect(Vec2 egress_start_field, Vec2 ingress_target_field, const OccupancyMap& occupancies) {
+    // Breitensuche, von Start, immer mit allen 4 (Conveyor3) + 4 (Conveyor4) + 4 (Combiner) Bewegungsmöglichkeiten
+    return std::nullopt;
+
+    // Invariante: Für Felder, die in der queue sind, gilt: Hier dürfen Inputs hin, für den gewünschten 
+    std::queue<Vec2> reached_ingestion_fields;
+
+    // TODO: Alle benachbarten Felder von egress_start_field in die Queue pushen, wenn nicht auch mit anderem Egress verbunden
+    // TODO auch: Vielleicht bei Verbindung zu anderem Egress warnen (wir erwarten nicht, dass das häufig passiert?)
+    // (Problem ist: +|.|+|)
+    // reached_ingestion_fields.push(start);
+
+
+    while(!reached_ingestion_fields.empty()) {
+      Vec2 reached_field = reached_ingestion_fields.front();
+      reached_ingestion_fields.pop();
+
+      // TODO: Ausprobieren
+      // 3-unit conveyor
+      for(Rotation rot = 0; rot < 4; ++rot) {
+        
+      }
+      // 4-unit conveyor
+      // combinern
+    }
   }
 }
