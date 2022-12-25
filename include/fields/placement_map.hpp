e2aa9ebecaf26125033c398b5b9e1d6d3ca46ad4
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2022-12-26 00:19:23 +0100

    Finalizes 1d scan in placement map

--- include/fields/placement_map.hpp
+++ include/fields/placement_map.hpp
@@ -24,32 +24,38 @@ inline std::tuple<PlacementMap, std::vector<Vec2>> placements_for(const Vec2 obj
                                                                   const DistanceMap& distance_map) {
   PlacementMap placements(occupancy_map.dimensions());
   std::vector<Vec2> possible_cells;
-  Coordinate last_unsuitable = -1;
+  Coordinate last_invalid = -1;
 
   for (auto y = 0; y < placements.dimensions().y(); ++y) {
     for (Coordinate i = 0 - object_dimensions.x(), j = -1; i < placements.dimensions().x();
          ++i, ++j) {
       if (occupancy_map.at(Vec2{j, y}) != CellOccupancy::EMPTY ||
           distance_map.at(Vec2{j, y}) == NOT_REACHABLE) {
-        last_unsuitable = j;
+        last_invalid = j;
       }
-      if (last_unsuitable < i) {
+      if (last_invalid < i) {
         placements.set(Vec2{i, y}, VALID);
       }
     }
   }
 
   for (auto x = 0; x < placements.dimensions().x(); ++x) {
-    for (Coordinate i = 0 - object_dimensions.y(), j = -1; i < placements.dimensions().y();
+    for (Coordinate j = -1; j < object_dimensions.y() - 1; ++j) {
+      if (placements.at(Vec2{x, j}) == INVALID) {
+        last_invalid = j;
+      }
+    }
+
+    for (Coordinate i = 0, j = object_dimensions.y() - 1; i < placements.dimensions().y();
          ++i, ++j) {
       if (placements.at(Vec2{x, j}) == INVALID) {
-        last_unsuitable = j;
+        last_invalid = j;
       }
-      if (last_unsuitable >= i && i >= 0) {
+      if (last_invalid < i) {
+        possible_cells.emplace_back(x, i);
+      } else {
         placements.set(Vec2{x, i}, INVALID);
-        continue;
       }
-      possible_cells.emplace_back(x, i);
     }
   }
 
