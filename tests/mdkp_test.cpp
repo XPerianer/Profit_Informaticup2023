b988d3d562dbe5130b4ec257106500fa2af8a66e
Dominik Meier <dominik.meier@student.hpi.de>
2023-01-03 17:06:37 +0100

    Make clang tidy happy

--- tests/mdkp_test.cpp
+++ tests/mdkp_test.cpp
@@ -14,21 +14,21 @@ using namespace profit;
 
 class PechTest : public testing::Test {
  protected:
-  Product goodProduct, badProduct;
+  Product good_product_, bad_product_;
 
   void SetUp() override {
-    goodProduct.points = 10;
-    goodProduct.requirements[ResourceType::TYPE0] = 2;
+    good_product_.points = 10;
+    good_product_.requirements[ResourceType::TYPE0] = 2;
 
     // Product_bad is bad because it has lower points than product_good
-    badProduct.points = 5;
-    badProduct.requirements[ResourceType::TYPE0] = 2;
+    bad_product_.points = 5;
+    bad_product_.requirements[ResourceType::TYPE0] = 2;
   }
 };
 
 TEST_F(PechTest, OutputsCorrectNumberOfElements) {
   AvailableResources resources = {8, 8, 8, 8, 8, 8, 8, 8};
-  auto output = pech(resources, {goodProduct});
+  auto output = pech(resources, {good_product_});
 
   std::vector<ProductCount> expected = {4};
   EXPECT_EQ(expected, output);
@@ -36,12 +36,12 @@ TEST_F(PechTest, OutputsCorrectNumberOfElements) {
 
 TEST_F(PechTest, ChoosesProductWithHigherScore) {
   AvailableResources resources = {8, 8, 8, 8, 8, 8, 8, 8};
-  auto output = pech(resources, {badProduct, goodProduct});
+  auto output = pech(resources, {bad_product_, good_product_});
   std::vector<ProductCount> expected = {0, 4};
   EXPECT_EQ(expected, output);
 
   // Swap parameters to know that we did not get lucky
-  output = pech(resources, {goodProduct, badProduct});
+  output = pech(resources, {good_product_, bad_product_});
   expected = {4, 0};
   EXPECT_EQ(expected, output);
 }
