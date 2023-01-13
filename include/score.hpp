3fffb0a337ca1d8faee9ce7e7a343b65223b6893
Dominik Meier <dominik.meier@student.hpi.de>
2023-01-13 19:26:32 +0100

    Implement score

---
+++ include/score.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <sys/types.h>
+
+#include "field_state.hpp"
+#include "io/parsing.hpp"
+#include "mdkp.hpp"
+using Turn = uint64_t;
+
+inline std::vector<Turn> when_are_deposits_empty(const profit::FieldState& state,
+                                                 const profit::parsing::Input& input) {
+  std::vector<Turn> when_empty(input.deposits.size());
+  std::vector<int> connected_pipelines(input.deposits.size());
+  for (const auto& [_, pipeline] : state.pipelines) {
+    connected_pipelines[pipeline.deposit_id]++;
+  }
+  for (unsigned int i = 0; i < input.deposits.size(); i++) {
+    const auto& deposit = input.deposits[i];
+    when_empty[i] =
+        static_cast<Turn>(profit::initial_resource_count(deposit)) / 3 * connected_pipelines[i];
+  }
+  return when_empty;
+}
+
+inline profit::ProductCount product_at_turn(const auto& pipeline, Turn empty_at, Turn turn) {
+  if (pipeline.parts.size() > turn) {
+    return 0;
+  }
+  if (turn > empty_at + pipeline.parts.size() + 1) {
+    return 3 * (empty_at + pipeline.parts.size() + 1);
+  }
+  return 3 * (turn - pipeline.parts.size() - 1);
+}
+
+inline profit::ProductScore score(const profit::FieldState& state, Turn turn,
+                                  const profit::parsing::Input& input) {
+  profit::ProductScore score = 0;
+  std::vector<Turn> when_empty = when_are_deposits_empty(state, input);
+  std::vector<profit::AvailableResources> resources_per_factory(state.factories.size());
+  for (const auto& [pipeline_id, pipeline] : state.pipelines) {
+    auto product_count = product_at_turn(pipeline, when_empty[pipeline.deposit_id], turn);
+    auto deposit = input.deposits[pipeline.deposit_id];
+    resources_per_factory[pipeline.factory_id][static_cast<char>(deposit.type)] += product_count;
+  }
+
+  for (const auto& [factory_id, factory] : state.factories) {
+    auto available_resources = resources_per_factory[factory_id];
+    score +=
+        profit::product_score(available_resources, input.products[static_cast<char>(factory.type)]);
+  }
+
+  return score;
+}
