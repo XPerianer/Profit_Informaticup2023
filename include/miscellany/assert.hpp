b52b20477cee708281e3a4593b4a9b1e1ca70256
Dominik Meier <dominik.meier@student.hpi.de>
2023-01-14 17:06:17 +0100

    clang the crab

--- include/miscellany/assert.hpp
+++ include/miscellany/assert.hpp
@@ -25,11 +25,12 @@
 #ifdef NDEBUG
 #define DEBUG(x)
 #else
-// NOLINTNEXTLINE(bugprone-macro-parentheses)
+// NOLINTBEGIN(bugprone-macro-parentheses)
 #define DEBUG(x)    \
   do {              \
     std::cerr << x; \
   } while (0)
 #endif
+// NOLINTEND(bugprone-macro-parentheses)
 
 // NOLINTEND(cppcoreguidelines-macro-usage)
